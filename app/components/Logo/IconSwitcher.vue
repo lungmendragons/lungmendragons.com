dc5e4e7032e81749069a76a86bf4c4c78d7fdbe0
tobo <36413368+toboruo@users.noreply.github.com>
2024-11-20 22:21:22 +0000

    fix: ref<Component> causes needless overhead

--- app/components/Logo/IconSwitcher.vue
+++ app/components/Logo/IconSwitcher.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
-import type { Component, CSSProperties } from "vue";
+import type { CSSProperties } from "vue";
 import { useDark } from "@vueuse/core";
 import LogoColorIcon from "~/components/SVG/LogoColorIcon_.vue";
 import LogoBlackIcon from "~/components/SVG/LogoBlackIcon_.vue";
 
 const { style } = defineProps<{ style?: CSSProperties }>();
 const isDark = useDark();
-const Logo = ref<Component>(LogoColorIcon);
-
-watch(isDark, (value: boolean) => {
-  Logo.value = value ? LogoColorIcon : LogoBlackIcon;
-});
 </script>
 
 <template>
-  <component :is="Logo" :width="style?.width" :height="style?.height" :style="style" />
+  <component
+    :is="isDark ? LogoColorIcon : LogoBlackIcon"
+    :width="style?.width"
+    :height="style?.height"
+    :style="style"
+  />
 </template>
