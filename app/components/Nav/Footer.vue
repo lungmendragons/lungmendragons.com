79f472140f9ee17d816ca1983ff218987dbe4420
tobo <36413368+toboruo@users.noreply.github.com>
2024-12-06 05:28:03 +0000

    add email to footer + center on mobile

--- app/components/Nav/Footer.vue
+++ app/components/Nav/Footer.vue
@@ -1,32 +1,40 @@
 <script setup>
 import Socials from "./Socials.vue";
+import Link from "~/components/Link.global.vue";
 import { useMediaQuery } from "@vueuse/core";
+import IcOutlineEmail from "~icons/ic/outline-email";
+
+const isMD = useMediaQuery(mediaQuery.minWidth.md);
 </script>
 
 <template>
-  <footer :style="{ justifyContent: 'center', marginTop: '1rem' }">
+  <footer style="margin-top: 1rem">
     <NFlex vertical>
-      <NDivider :style="{ margin: '0' }" />
+      <NDivider style="margin: 0" />
       <NFlex
-        justify="space-between"
         align="center"
-        :style="{ paddingTop: '0.5rem', paddingBottom: '1rem' }">
-        <div
-          :style="{
-            margin: useMediaQuery(mediaQuery.minWidth.md) ? '0' : '0 auto',
-            textAlign: 'center',
-            fontSize: '0.75rem',
-            lineHeight: '1rem',
-          }">
-          &copy; 2024 Lungmen Dragons. All rights reserved.
-        </div>
-        <Socials
-          :style="{
-            margin: useMediaQuery(mediaQuery.minWidth.md) ? '0' : '0 auto',
-            width: '12rem',
-            padding: '0.5rem 0.25rem',
-          }"
-        />
+        style="padding-top: 0.5rem; padding-bottom: 1rem"
+        :justify="isMD ? 'space-between' : 'center'">
+        <NFlex
+          vertical
+          style="font-size: 0.75rem"
+          :size="2">
+          <span>
+            &copy; 2024 Lungmen Dragons. All rights reserved.
+          </span>
+          <NFlex
+            align="center"
+            :justify="isMD ? '' : 'center'"
+            :size="4">
+            <NIcon size="medium">
+              <IcOutlineEmail />
+            </NIcon>
+            <Link to="mailto:lungmendragons@gmail.com">
+              lungmendragons@gmail.com
+            </Link>
+          </NFlex>
+        </NFlex>
+        <Socials style="margin-top: 4px; width: 12rem" />
       </NFlex>
     </NFlex>
   </footer>
