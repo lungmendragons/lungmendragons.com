029968cc5e12fa39a9626c06e2697074a3bd6cb5
tobo <36413368+toboruo@users.noreply.github.com>
2024-12-15 18:16:25 +0000

    fix: blob not serving images

--- server/routes/images/[...pathname].get.ts
+++ server/routes/images/[...pathname].get.ts
@@ -1,9 +1,5 @@
-export default cachedEventHandler(async (event) => {
-  const { pathname } = event.context.params || {};
-
-  // setHeader(event, "Content-Security-Policy", "default-src 'none';");
+export default eventHandler(async (event) => {
+  const { pathname } = getRouterParams(event);
+  setHeader(event, "Content-Security-Policy", "default-src 'none';");
   return hubBlob().serve(event, pathname);
-}, {
-  maxAge: 3600, // 1 hour
-  getKey: event => event.path,
 });
