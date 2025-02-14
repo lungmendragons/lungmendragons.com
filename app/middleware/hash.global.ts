b1793337912be642744b29ab1289d44ef97f9edb
tobo <36413368+toboruo@users.noreply.github.com>
2025-02-14 17:09:16 +0000

    urgent redirect

--- app/middleware/hash.global.ts
+++ app/middleware/hash.global.ts
@@ -1,5 +1,6 @@
 export default defineNuxtRouteMiddleware((to) => {
-  if (to.path === "/" && to.fullPath.startsWith("/#/")) {
+  if (to.path === "/" && to.fullPath.startsWith("/#/"))
     return navigateTo(to.hash.slice(1));
-  }
+  if (to.path === "/sgl")
+    return navigateTo("/sgl2");
 });
