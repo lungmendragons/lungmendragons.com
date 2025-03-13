cb1910472c422048e4b01643cdab9b3f98a2dbf5
tobo <36413368+toboruo@users.noreply.github.com>
2025-03-13 03:40:30 +0000

    temp sgl schedule

--- app/pages/(about)/(events)/sgl2.vue
+++ app/pages/(about)/(events)/sgl2.vue
@@ -1,25 +1,25 @@
 <script setup lang="ts">
 import { useMediaQuery } from "@vueuse/core";
-import SGLRegListItem from "~/components/SGL/RegListItem.vue";
+// import SGLRegListItem from "~/components/SGL/RegListItem.vue";
 import SGLQFListItem from "~/components/SGL/QFListItem.vue";
-import type { RowData, RowDataSorted, RegData } from "~/utils/events-archive/sgl2";
+import type { RowData, RowDataSorted, /* RegData */ } from "~/utils/events-archive/sgl2";
 import PhEmptyBold from '~icons/ph/empty-bold?width=256px&height=256px';
 
-const { client } = useAuth();
-const { data: session } = await client.useSession(useFetch);
-const message = useMessage();
+// const { client } = useAuth();
+// const { data: session } = await client.useSession(useFetch);
+// const message = useMessage();
 
 const isMD = useMediaQuery(mediaQuery.minWidth.md as string);
 const data = await $fetch("/api/pages/sgl2/all", { method: "GET" });
-const isLoading = ref(false);
+// const isLoading = ref(false);
 const qf = data.qf as RowData[];
-const reg = data.reg as RegData[];
+// const reg = data.reg as RegData[];
 
-function regSort(): RegData[] {
-  return reg
-    .filter((entry) => entry.valid)
-    .sort((a, b) => a.player.localeCompare(b.player));
-}
+// function regSort(): RegData[] {
+//   return reg
+//     .filter((entry) => entry.valid)
+//     .sort((a, b) => a.player.localeCompare(b.player));
+// }
 
 function qfSort(): RowDataSorted[] {
   return qf
@@ -33,19 +33,19 @@ function qfSort(): RowDataSorted[] {
     });
 }
 
-function doGsheetUpdate() {
-  isLoading.value = true;
-  $fetch("/api/pages/sgl2/gsheet", { method: "PUT" })
-    .then(res => {
-      if (res === "success") {
-        isLoading.value = false;
-        message.success("Data updated successfully");
-      } else {
-        isLoading.value = false;
-        message.error("Failed to update data");
-      }
-    });
-}
+// function doGsheetUpdate() {
+//   isLoading.value = true;
+//   $fetch("/api/pages/sgl2/gsheet", { method: "PUT" })
+//     .then(res => {
+//       if (res === "success") {
+//         isLoading.value = false;
+//         message.success("Data updated successfully");
+//       } else {
+//         isLoading.value = false;
+//         message.error("Failed to update data");
+//       }
+//     });
+// }
 
 onBeforeMount(() => qfSort());
 </script>
@@ -74,18 +74,18 @@ onBeforeMount(() => qfSort());
           Registration ends:
           <NTime
             :time="1740034740000"
-            format="yyyy-MM-dd hh:mm"
+            format="yyyy-MM-dd HH:mm"
             class="font-bold"
           />
           <span class="italic">
             (<NTime :time="1740034740000" type="relative" />)
           </span>
-        </div> -->
+        </div>
         <div class="mx-4 mt-2 text-xs lg:text-sm text-center text-white drop-shadow-[0_0_1px_black]">
           Live qualifiers end:
           <NTime
             :time="1740639540000"
-            format="yyyy-MM-dd hh:mm"
+            format="yyyy-MM-dd HH:mm"
             class="font-bold"
           />
           <span class="italic">
@@ -94,13 +94,359 @@ onBeforeMount(() => qfSort());
         </div>
         <div class="mx-4 italic text-xs text-center text-white drop-shadow-[0_0_1px_black]">
           Shown in your timezone: [{{ Intl.DateTimeFormat().resolvedOptions().timeZone }}]
-        </div>
+        </div> -->
+        <NFlex vertical class="mx-auto pb-8 w-full md:w-3/5 p-2 md:p-4">
+          <NuxtLink
+            class="mx-auto"
+            to="https://www.youtube.com/@LungmenDragons/streams"
+            target="_blank">
+            <NButton
+              :size="isMD ? 'large' : 'medium'"
+              type="primary"
+              icon-placement="right"
+              color="#F24242">
+              <template #icon>
+                <ExternalLinkIcon :size="isMD ? 'large' : 'medium'" />
+              </template>
+              <span class="font-bold">Watch live on YouTube</span>
+            </NButton>
+          </NuxtLink>
+          <NTable :single-line="false" size="small">
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Time</th>
+                <th>Server</th>
+                <th>Player</th>
+                <th>Group</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td rowspan="3">
+                  <NTime
+                    time-zone="America/New_York"
+                    :time="1741973400000"
+                    format="yyyy-MM-dd"
+                    class="font-bold"
+                  />
+                </td>
+                <td>
+                  <NTime
+                    time-zone="America/New_York"
+                    :time="1741973400000"
+                    format="HH:mm"
+                    class="font-bold"
+                  />
+                </td>
+                <td>TC</td>
+                <td>Liriyin</td>
+                <td>D</td>
+              </tr>
+              <tr>
+                <td>
+                  <NTime
+                    time-zone="America/New_York"
+                    :time="1741986000000"
+                    format="HH:mm"
+                    class="font-bold"
+                  />
+                </td>
+                <td>KR</td>
+                <td>Artuosa</td>
+                <td>A</td>
+              </tr>
+              <tr>
+                <td>
+                  <NTime
+                    time-zone="America/New_York"
+                    :time="1741998600000"
+                    format="HH:mm"
+                    class="font-bold"
+                  />
+                </td>
+                <td>KR</td>
+                <td>Hays</td>
+                <td>C</td>
+              </tr>
+              <tr>
+                <td rowspan="3">
+                  <NTime
+                    time-zone="America/New_York"
+                    :time="1742059800000"
+                    format="yyyy-MM-dd"
+                    class="font-bold"
+                  />
+                </td>
+                <td>
+                  <NTime
+                    time-zone="America/New_York"
+                    :time="1742059800000"
+                    format="HH:mm"
+                    class="font-bold"
+                  />
+                </td>
+                <td>EN</td>
+                <td>Aoao</td>
+                <td>A</td>
+              </tr>
+              <tr>
+                <td>
+                  <NTime
+                    time-zone="America/New_York"
+                    :time="1742083200000"
+                    format="HH:mm"
+                    class="font-bold"
+                  />
+                </td>
+                <td>SC</td>
+                <td>Cola</td>
+                <td>B</td>
+              </tr>
+              <tr>
+                <td>
+                  <NTime
+                    time-zone="America/New_York"
+                    :time="1742092200000"
+                    format="HH:mm"
+                    class="font-bold"
+                  />
+                </td>
+                <td>JP</td>
+                <td>Sena</td>
+                <td>D</td>
+              </tr>
+              <tr>
+                <td rowspan="3">
+                  <NTime
+                    time-zone="America/New_York"
+                    :time="1742146200000"
+                    format="yyyy-MM-dd"
+                    class="font-bold"
+                  />
+                </td>
+                <td>
+                  <NTime
+                    time-zone="America/New_York"
+                    :time="1742146200000"
+                    format="HH:mm"
+                    class="font-bold"
+                  />
+                </td>
+                <td>TC</td>
+                <td>Winsok</td>
+                <td>A</td>
+              </tr>
+              <tr>
+                <td>
+                  <NTime
+                    time-zone="America/New_York"
+                    :time="1742169600000"
+                    format="HH:mm"
+                    class="font-bold"
+                  />
+                </td>
+                <td>KR</td>
+                <td>Hijkstuv</td>
+                <td>B</td>
+              </tr>
+              <tr>
+                <td>
+                  <NTime
+                    time-zone="America/New_York"
+                    :time="1742178600000"
+                    format="HH:mm"
+                    class="font-bold"
+                  />
+                </td>
+                <td>JP</td>
+                <td>YS</td>
+                <td>A</td>
+              </tr>
+              <tr>
+                <td rowspan="3">
+                  <NTime
+                    time-zone="America/New_York"
+                    :time="1742232600000"
+                    format="yyyy-MM-dd"
+                    class="font-bold"
+                  />
+                </td>
+                <td>
+                  <NTime
+                    time-zone="America/New_York"
+                    :time="1742232600000"
+                    format="HH:mm"
+                    class="font-bold"
+                  />
+                </td>
+                <td>EN</td>
+                <td>Mei of May</td>
+                <td>A</td>
+              </tr>
+              <tr>
+                <td>
+                  <NTime
+                    time-zone="America/New_York"
+                    :time="1742256000000"
+                    format="HH:mm"
+                    class="font-bold"
+                  />
+                </td>
+                <td>TC</td>
+                <td>WXY</td>
+                <td>B</td>
+              </tr>
+              <tr>
+                <td>
+                  <NTime
+                    time-zone="America/New_York"
+                    :time="1742265000000"
+                    format="HH:mm"
+                    class="font-bold"
+                  />
+                </td>
+                <td>SC</td>
+                <td>Kaisei</td>
+                <td>A</td>
+              </tr>
+              <tr>
+                <td rowspan="3">
+                  <NTime
+                    time-zone="America/New_York"
+                    :time="1742319000000"
+                    format="yyyy-MM-dd"
+                    class="font-bold"
+                  />
+                </td>
+                <td>
+                  <NTime
+                    time-zone="America/New_York"
+                    :time="1742319000000"
+                    format="HH:mm"
+                    class="font-bold"
+                  />
+                </td>
+                <td>EN</td>
+                <td>Polymer</td>
+                <td>C</td>
+              </tr>
+              <tr>
+                <td>
+                  <NTime
+                    time-zone="America/New_York"
+                    :time="1742342400000"
+                    format="HH:mm"
+                    class="font-bold"
+                  />
+                </td>
+                <td>TC</td>
+                <td>Toby</td>
+                <td>B</td>
+              </tr>
+              <tr>
+                <td>
+                  <NTime
+                    time-zone="America/New_York"
+                    :time="1742351400000"
+                    format="HH:mm"
+                    class="font-bold"
+                  />
+                </td>
+                <td>JP</td>
+                <td>Caron</td>
+                <td>C</td>
+              </tr>
+              <tr>
+                <td rowspan="3">
+                  <NTime
+                    time-zone="America/New_York"
+                    :time="1742405400000"
+                    format="yyyy-MM-dd"
+                    class="font-bold"
+                  />
+                </td>
+                <td>
+                  <NTime
+                    time-zone="America/New_York"
+                    :time="1742405400000"
+                    format="HH:mm"
+                    class="font-bold"
+                  />
+                </td>
+                <td>EN</td>
+                <td>Bait</td>
+                <td>D</td>
+              </tr>
+              <tr>
+                <td>
+                  <NTime
+                    time-zone="America/New_York"
+                    :time="1742428800000"
+                    format="HH:mm"
+                    class="font-bold"
+                  />
+                </td>
+                <td>KR</td>
+                <td>LemuenWheelchairThief</td>
+                <td>D</td>
+              </tr>
+              <tr>
+                <td>
+                  <NTime
+                    time-zone="America/New_York"
+                    :time="1742437800000"
+                    format="HH:mm"
+                    class="font-bold"
+                  />
+                </td>
+                <td>SC</td>
+                <td>Eyanx</td>
+                <td>C</td>
+              </tr>
+              <tr>
+                <td rowspan="2">
+                  <NTime
+                    time-zone="America/New_York"
+                    :time="1742491800000"
+                    format="yyyy-MM-dd"
+                    class="font-bold"
+                  />
+                </td>
+                <td>
+                  <NTime
+                    time-zone="America/New_York"
+                    :time="1742491800000"
+                    format="HH:mm"
+                    class="font-bold"
+                  />
+                </td>
+                <td>SC</td>
+                <td>Soup</td>
+                <td>A</td>
+              </tr>
+              <tr>
+                <td>
+                  <NTime
+                    time-zone="America/New_York"
+                    :time="1742515200000"
+                    format="HH:mm"
+                    class="font-bold"
+                  />
+                </td>
+                <td>JP</td>
+                <td>Miguel_San</td>
+                <td>B</td>
+              </tr>
+            </tbody>
+          </NTable>
+        </NFlex>
       </div>
       <NFlex
         justify="center"
         align="center"
         class="text-xs md:text-sm my-4">
-        <NuxtLink to="https://forms.gle/2idKuHk9pK5nVGnf9" target="_blank">
+        <!-- <NuxtLink to="https://forms.gle/2idKuHk9pK5nVGnf9" target="_blank">
           <NButton
             :size="isMD ? 'medium' : 'small'"
             type="primary"
@@ -110,7 +456,7 @@ onBeforeMount(() => qfSort());
             </template>
             Register
           </NButton>
-        </NuxtLink>
+        </NuxtLink> -->
         <NuxtLink to="https://docs.google.com/document/d/1TteQ9YokVLr83krA5S2GBjfrz7bN2D1f5F3PndA2Ktc" target="_blank">
           <NButton
             :size="isMD ? 'medium' : 'small'"
@@ -119,11 +465,11 @@ onBeforeMount(() => qfSort());
             <template #icon>
               <ExternalLinkIcon :size="isMD ? 'large' : 'medium'" />
             </template>
-            Rules
+            Qualifying Rules
           </NButton>
         </NuxtLink>
       </NFlex>
-      <NCard
+      <!-- <NCard
         v-if="session && (session.user.permissions & 4)"
         class="w-fit mx-auto my-4"
         size="small">
@@ -140,38 +486,39 @@ onBeforeMount(() => qfSort());
         <div class="text-xs text-center w-full">
           Data is cached, allow a few mins <br> for changes to appear
         </div>
-      </NCard>
-      <NTabs
+      </NCard> -->
+      <!-- <NTabs
         type="segment"
         animated
         justify-content="space-evenly"
         class="mx-auto pb-8 w-full md:w-4/5 px-2 md:px-4"
         tab-style="filter:drop-shadow(0 1px 3px black)">
-        <NTabPane name="qualifiers" tab="Live Qualifiers">
-          <NFlex vertical class="mx-auto">
-            <NList bordered>
-              <NListItem v-if="qfSort().length === 0">
-                <NEmpty description="No data yet.">
-                  <template #icon>
-                    <n-icon>
-                      <PhEmptyBold />
-                    </n-icon>
-                  </template>
-                </NEmpty>
-              </NListItem>
-              <NListItem
-                v-else
-                v-for="entry in qfSort()"
-                :key="entry.key"
-                :style="{
-                  backgroundColor: entry.rank % 2 === 0 ? '#5552' : '',
-                  padding: '0.625rem 0.875rem 0.75rem 0.875rem',
-                }">
-                <SGLQFListItem :data="entry" />
-              </NListItem>
-            </NList>
-          </NFlex>
-        </NTabPane>
+        <NTabPane name="qualifiers" tab="Live Qualifiers"> -->
+      <NFlex vertical class="mx-auto pb-8 w-full md:w-3/5 px-2 md:px-4">
+        <div>Qualifying Results</div>
+        <NList bordered>
+          <NListItem v-if="qfSort().length === 0">
+            <NEmpty description="No data yet.">
+              <template #icon>
+                <n-icon>
+                  <PhEmptyBold />
+                </n-icon>
+              </template>
+            </NEmpty>
+          </NListItem>
+          <NListItem
+            v-else
+            v-for="entry in qfSort()"
+            :key="entry.key"
+            :style="{
+              backgroundColor: entry.rank % 2 === 0 ? '#5552' : '',
+              padding: '0.625rem 0.875rem 0.75rem 0.875rem',
+            }">
+            <SGLQFListItem :data="entry" />
+          </NListItem>
+        </NList>
+      </NFlex>
+      <!-- </NTabPane>
         <NTabPane name="registered" tab="Registered Players">
           <NFlex vertical class="mx-auto w-full md:w-2/3">
             <NList bordered>
@@ -197,7 +544,7 @@ onBeforeMount(() => qfSort());
             </NList>
           </NFlex>
         </NTabPane>
-      </NTabs>
+      </NTabs> -->
     </NScrollbar>
   </div>
 </template>
