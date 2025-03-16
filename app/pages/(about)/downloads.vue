18af9ce00e1d66f3d184b0eda40c060ca0e413c2
Function-0 <ibrahimj789@gmail.com>
2025-03-16 05:04:54 -0400

    Downloads - Add Lung Bao concept art

--- app/pages/(about)/downloads.vue
+++ app/pages/(about)/downloads.vue
@@ -26,6 +26,16 @@
           LB_IS4_4thEND_SGL2.png<br>4800x3600px, 20.7 MB
         </div>
       </NCard>
+      <NCard class="max-w-[300px] h-[250px]" content-style="padding:8px">
+        <template #cover>
+          <a download href="/images/ld/lb/0_CONCEPTART_LUNGBAO_ORIGINAL.png">
+            <NuxtImg src="https://lungmendragons.com/images/ld/lb/0_CONCEPTART_LUNGBAO_ORIGINAL.png" :height="200" />
+          </a>
+        </template>
+        <div class="text-xs text-center">
+          0_CONCEPTART_LUNGBAO_ORIGINAL.png<br>5355x2665px, 4.84 MB
+        </div>
+      </NCard>
     </NFlex>
   </NFlex>
 </template>
