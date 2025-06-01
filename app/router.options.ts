912a3a2a40ab19336c093edabee3d83c5e53a617
Yurihaia <17830663+Yurihaia@users.noreply.github.com>
2025-05-31 20:52:59 -0400

    Reimplement subdomain scanning + move the board source back to the original url.

--- app/router.options.ts
+++ app/router.options.ts
@@ -2,7 +2,33 @@ import type { RouterOptions } from "@nuxt/schema";
 
 // https://router.vuejs.org/api/interfaces/routeroptions.html
 export default <RouterOptions>{
-  routes: (_routes) => {},
+  routes: (routes) => {
+    const { ssrContext } = useNuxtApp();
+    const subdomain = useSubdomain();
+    if (ssrContext?.event.context.subdomain)
+      subdomain.value = ssrContext?.event.context.subdomain;
+
+    if (subdomain.value) {
+      const path = `/subdomain/${subdomain.value}/`;
+      const index = path.slice(0, path.length - 1);
+      return routes.values().map((v) => {
+        let newPath: string;
+        if (v.path.startsWith(path)) {
+          newPath = v.path.slice(path.length - 1);
+        } else if (v.path === index) {
+          newPath = "/";
+        } else {
+          return undefined;
+        }
+        return {
+          ...v,
+          path: newPath,
+        };
+      }).filter(v => !!v).toArray();
+    } else {
+      return routes.filter(v => !v.path.startsWith("/subdomain/"));
+    }
+  },
 
   // scrollBehavior(to, from, savedPosition) {
   //   if (savedPosition) return savedPosition
